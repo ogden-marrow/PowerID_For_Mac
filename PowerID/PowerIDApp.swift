3a602b49c025653e8922c08ae03727ce62b5cd5e
John E. Palenchar <jpalenchar.3@gmail.com>
2026-01-16 12:49:56 -0500

    Initial Commit

---
+++ PowerID/PowerIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerIDApp.swift
+//  PowerID
+//
+//  Created by John Palenchar on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct PowerIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
